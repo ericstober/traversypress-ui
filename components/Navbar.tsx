64999e5d938cfd7e970063f3a6f434c96a67aa54
ericstober <erictstober@gmail.com>
2024-08-13 13:32:24 -0500

    added dropdown to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "../img/logo.png";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 const Navbar = () => {
   return (
@@ -10,10 +11,27 @@ const Navbar = () => {
         <Image src={logo} alt='TraversyPress' width={40} />
       </Link>
 
-      <Avatar>
-        <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
-        <AvatarFallback className='text-black'>P</AvatarFallback>
-      </Avatar>
+      <DropdownMenu>
+        <DropdownMenuTrigger className='focus:outline-none'>
+          <Avatar>
+            <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
+            <AvatarFallback className='text-black'>P</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem>
+            <Link href='/profile'>Profile</Link>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem>
+            <Link href='/auth'>Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
