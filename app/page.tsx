9c323a3063ffb77415010aff5c73b919d3daf733
ericstober <erictstober@gmail.com>
2024-08-15 11:19:13 -0500

    Added AnalyticsChart component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import DashboardCard from "@/components/dashboard/DashboardCard";
 import PostsTable from "@/components/posts/PostsTable";
+import AnalyticsChart from "@/components/dashboard/AnalyticsChart";
 import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
         <DashboardCard title='Users' count={750} icon={<User className='text-slate-500' size={72} />} />
         <DashboardCard title='Comments' count={1200} icon={<MessageCircle className='text-slate-500' size={72} />} />
       </div>
+      <AnalyticsChart />
       <PostsTable title='Latest Posts' limit={5} />
     </>
   );
