beedd6efb9fccd2c344d3e5b94fd2743fbcb5546
ericstober <erictstober@gmail.com>
2024-08-16 09:32:45 -0500

    added AuthTabs, LoginForm and RegisterForm components

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -1,5 +1,11 @@
+import AuthTabs from "@/components/auth/AuthTabs";
+
 const AuthPage = () => {
-  return <div>auth</div>;
+  return (
+    <>
+      <AuthTabs />
+    </>
+  );
 };
 
 export default AuthPage;
